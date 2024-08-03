0fcfb59e267c24d3a487f437e6ef22efc1a6bc4d
MrJeremyFisher <jeremy@generaleclectic.org>
2024-08-02 23:31:10 -0400

    Armadillo

--- common/src/main/java/com/aleksey/combatradar/config/RadarConfig.java
+++ common/src/main/java/com/aleksey/combatradar/config/RadarConfig.java
@@ -8,6 +8,7 @@ import net.minecraft.world.entity.GlowSquid;
 import net.minecraft.world.entity.ambient.Bat;
 import net.minecraft.world.entity.animal.*;
 import net.minecraft.world.entity.animal.allay.Allay;
+import net.minecraft.world.entity.animal.armadillo.Armadillo;
 import net.minecraft.world.entity.animal.axolotl.Axolotl;
 import net.minecraft.world.entity.animal.camel.Camel;
 import net.minecraft.world.entity.animal.frog.Frog;
@@ -169,16 +170,16 @@ public class RadarConfig {
         _entityList.add(new RadarEntityInfo(ZombieVillager.class, "Zombie Villager", "icons/zombie_villager.png", GroupType.AGGRESSIVE));
         _entityList.add(new RadarEntityInfo(EnderDragon.class, "Ender Dragon", "icons/dragon.png", GroupType.AGGRESSIVE));
 
-        // Update to 1.19.4
         _entityList.add(new RadarEntityInfo(Allay.class, "Allay", "icons/allay.png", GroupType.NEUTRAL));
         _entityList.add(new RadarEntityInfo(Frog.class, "Frog", "icons/frog.png", GroupType.NEUTRAL));
         _entityList.add(new RadarEntityInfo(Tadpole.class, "Tadpole", "icons/tadpole.png", GroupType.NEUTRAL));
         _entityList.add(new RadarEntityInfo(Warden.class, "Warden", "icons/warden.png", GroupType.AGGRESSIVE));
         _entityList.add(new RadarEntityInfo(ChestBoat.class, "Boat With Chest", "icons/boat_chest.png", GroupType.OTHER));
 
-        // Update to 1.20.1
         _entityList.add(new RadarEntityInfo(Camel.class, "Camel", "icons/camel.png", GroupType.NEUTRAL));
         _entityList.add(new RadarEntityInfo(Sniffer.class, "Sniffer", "icons/sniffer.png", GroupType.NEUTRAL));
+
+        _entityList.add(new RadarEntityInfo(Armadillo.class, "Armadillo", "icons/armadillo.png", GroupType.NEUTRAL));
 
         _entityList.sort(new RadarEntityInfo.EntityComparator());
 
